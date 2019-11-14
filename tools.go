7bbda0157b3cfebcce1214030934a8604d506390
Terry Ding <terryding77@gmail.com>
2019-11-14 10:37:05 +0800

    temp remove gometalint tool to pass the go 1.13 compile

--- tools.go
+++ tools.go
@@ -3,7 +3,7 @@
 package tools
 
 import (
-	"github.com/alecthomas/gometalinter"
+    // "github.com/alecthomas/gometalinter"
 	"github.com/client9/misspell/cmd/misspell" // lint
 	"github.com/gordonklaus/ineffassign"       // lint
 	"github.com/tsenart/deadcode"              // lint
